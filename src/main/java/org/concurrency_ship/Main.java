82f629b3deb1cbece1fbd616d941d35ed1b2d2fc
SergeySmirnovJava <sergej.smirnov1998@yandex.ru>
2021-08-19 04:32:58 +0300

    Main class

--- src/main/java/org/concurrency_ship/Main.java
+++ src/main/java/org/concurrency_ship/Main.java
@@ -1,7 +1,13 @@
 package org.concurrency_ship;
 
+import java.util.Scanner;
+import java.util.concurrent.ExecutionException;
+
 public class Main {
-    public static void main(String[] args) {
-        
+    public static void main(String[] args) throws ExecutionException, InterruptedException {
+        Scanner in = new Scanner(System.in);
+
+        ShipGenerator shipGenerator = new ShipGenerator(in.nextInt());
+        shipGenerator.launchShips();
     }
 }
